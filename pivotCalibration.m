cccae85df5264939466c56f4f5f724d3a7914c49
Harshin Sanam <65623909+harshnath29@users.noreply.github.com>
2025-03-30 10:44:30 -0500

    Incorporate rotation

--- pivotCalibration.m
+++ pivotCalibration.m
@@ -1,29 +1,34 @@
 function [tipPosition, dimplePosition] = pivotCalibration(markerFrames)
     % Input:
-    % markerFrames: A cell array where each element is an Nx3 matrix
-    %               containing the positions of N EM markers for one frame.
+    % markerFrames: Cell array of EM marker positions (Nx3 matrices) per frame
     % Output:
-    % tipPosition: 3x1 vector representing the position of the tool tip (pivot point).
-    % dimplePosition: 3x1 vector representing the position of the dimple.
+    % tipPosition: 3x1 vector (tip position in probe coordinates)
+    % dimplePosition: 3x1 vector (dimple position in tracker coordinates)
 
     numFrames = length(markerFrames);
     midpoints = zeros(numFrames, 3);
-    translatedMarkers = cell(numFrames, 1);
+    R = cell(numFrames, 1);
+
+    refFrame = markerFrames{1};
+    refMidpoint = mean(refFrame, 1);
+    centeredRef = refFrame - refMidpoint;
 
     for k = 1:numFrames
         midpoints(k, :) = mean(markerFrames{k}, 1);
-        translatedMarkers{k} = markerFrames{k} - midpoints(k, :);
+        centeredMoving = markerFrames{k} - midpoints(k, :);
+
+        [R_k, ~] = pointSetRegistration(centeredMoving, centeredRef);
+        R{k} = R_k;
     end
 
     A = zeros(3 * numFrames, 3);
     b = zeros(3 * numFrames, 1);
 
     for k = 1:numFrames
-        A(3*k-2:3*k, :) = eye(3);
-        b(3*k-2:3*k) = midpoints(k, :)'; % Midpoint contribution
+        A(3*k-2:3*k, :) = R{k}; % Incorporate rotation matrix
+        b(3*k-2:3*k) = midpoints(k, :)';
     end
 
     tipPosition = A \ b;
     dimplePosition = mean(midpoints, 1)' + tipPosition;
-
 end
