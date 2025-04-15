b5f44b1d6ab39f358e74e166ac184f225628b8e1
Rahul Menon <menonrahul@gmail.com>
2025-04-15 13:55:03 -0500

    do noisy data things for p2

--- runEyeInHand.m
+++ runEyeInHand.m
@@ -3,3 +3,9 @@
 
 [q_Robot_config, q_camera_config,t_Robot_config,t_camera_config ]=data_quaternion_noisy();
 [R, p] = eyeInHand(q_Robot_config, t_Robot_config, q_camera_config, t_camera_config)
+
+[q_Robot_config, q_camera_config,t_Robot_config,t_camera_config ]=data_quaternion();
+[R, p] = eyeInHand(q_Robot_config(1:5, :), t_Robot_config(1:5, :), q_camera_config(1:5, :), t_camera_config(1:5, :))
+
+[q_Robot_config, q_camera_config,t_Robot_config,t_camera_config ]=data_quaternion_noisy();
+[R, p] = eyeInHand(q_Robot_config(1:5, :), t_Robot_config(1:5, :), q_camera_config(1:5, :), t_camera_config(1:5, :))
